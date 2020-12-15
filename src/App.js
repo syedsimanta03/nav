eab50998cfd6bf1d7b76e90d98d38a07a1ebc22e
sam <syed.simanta06@gmail.com>
2020-12-15 12:01:36 +0600

    'added'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='navbar'>
+      <div className='option-set-one'>
+        <i className='ri-menu-line'></i>
+        <div className='info'>
+          <strong> Item 1 of 10 </strong>
+          <p> Question Id: 968 </p>
+        </div>
+        <div className='mark'>
+          <input type='checkbox' name='check' id='checkbox' />
+          <i className='ri-flag-2-line'></i>
+          <p className='inline'>Mark</p>
+        </div>
+      </div>
+      <div className='option-set-two'>
+        <div className='prev'>
+          <i className='ri-skip-back-line'></i>
+          <p>Previous</p>
+        </div>
+        <div className='next'>
+          <i className='ri-skip-forward-line'></i>
+          <p>Next</p>
+        </div>
+      </div>
+      <div className='option-set-three'>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+        <div className='icon'>
+          <i className='ri-apps-fill'></i>
+          <p>Next</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
